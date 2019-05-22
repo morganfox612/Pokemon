ec87f520e7d5ce488a0d864100daa098e3e5b7ce
Morgan Fox <morganfox61297@gmail.com>
2019-05-22 16:50:40 -0400

    I am trying so hard to fix this background image

--- main.js
+++ main.js
@@ -11,27 +11,26 @@ requestAPI = () => {
                 defense: data['stats'][3]['base_stat'],
                 hp: data['stats'][5]['base_stat'],
                 ability1: data['abilities'][0]['ability']['name'],
-                ability2: data['abilities'][1]['ability']['name']
-
-
+                ability2: data['abilities'][1]['ability']['name'],
+                sprite: data['sprites']['front_default']
 
             }
         }
     };
     xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${Pokemon}.json`, true);
     xhttp.send();
-}
 
+  }
 requestAPI()
 
 displayPokemon = () => {
     let cont = document.createElement('div')
     let h1 = document.createElement('h1')
-    let p1 = document.createElement('p')
-    let p2 = document.createElement('p')
-    let p3 = document.createElement('p')
-    let p4 = document.createElement('p')
-    let p5 = document.createElement('p')
+    let p1 = document.createElement('p1')
+    let p2 = document.createElement('p2')
+    let p3 = document.createElement('p3')
+    let p4 = document.createElement('p4')
+    let p5 = document.createElement('p5')
     let img = document.createElement('img')
 
     h1.innerText = pokemon['name']
@@ -40,7 +39,7 @@ displayPokemon = () => {
     p3.innerText = pokemon['hp']
     p4.innerText = pokemon['ability1']
     p5.innerText = pokemon['ability2']
-    img.setAttribute('src', `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${Pokemon}.png`)
+    img.setAttribute('src', pokemon['sprite'])
     cont.appendChild(h1)
     cont.appendChild(p1)
     cont.appendChild(p2)
@@ -50,3 +49,5 @@ displayPokemon = () => {
     cont.appendChild(img)
     document.body.appendChild(cont)
 }
+
+setTimeout(displayPokemon, 200)
