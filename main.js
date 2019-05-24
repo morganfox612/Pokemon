d1ba9411c08025798c4448722b80b876ed10eb35
Morgan Fox <morganfox61297@gmail.com>
2019-05-24 16:36:27 -0400

    Finally got freaking mew to show up

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 requestAPI = () => {
   const Pokemon = prompt("Enter a pokemon name")
-  // var pokemon = prompt("enter pokemon name")
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -10,19 +9,17 @@ requestAPI = () => {
                 attack: data['stats'][4]['base_stat'],
                 defense: data['stats'][3]['base_stat'],
                 hp: data['stats'][5]['base_stat'],
-                ability1: data['abilities'][0]['ability']['name'],
-                ability2: data['abilities'][1]['ability']['name'],
+                ability: data['abilities'][0]['ability']['name'],
                 sprite: data['sprites']['front_default']
 
             }
+            displayPokemon()
         }
     };
-    xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${Pokemon}.json`, true);
+    xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${Pokemon.toLowerCase()}.json`, true);
     xhttp.send();
 
-    setTimeout(displayPokemon, 200)
   }
-// requestAPI()
 
 let cont = document.createElement('div')
 displayPokemon = () => {
@@ -31,22 +28,19 @@ displayPokemon = () => {
     let p2 = document.createElement('p2')
     let p3 = document.createElement('p3')
     let p4 = document.createElement('p4')
-    let p5 = document.createElement('p5')
     let img = document.createElement('img')
 
     h1.innerText = pokemon['name']
     p1.innerText = pokemon['defense']
     p2.innerText = pokemon['attack']
     p3.innerText = pokemon['hp']
-    p4.innerText = pokemon['ability1']
-    p5.innerText = pokemon['ability2']
+    p4.innerText = pokemon['ability']
     img.setAttribute('src', pokemon['sprite'])
     cont.appendChild(h1)
     cont.appendChild(p1)
     cont.appendChild(p2)
     cont.appendChild(p3)
     cont.appendChild(p4)
-    cont.appendChild(p5)
     cont.appendChild(img)
     document.body.appendChild(cont)
 }
